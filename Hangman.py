a185a9522e7265c7d0227da46ce1d37b1ad16a46
Haebin Yoo <h.yoo@students.chapelfield.org>
2019-06-06 20:07:32 -0400

    Fix typo and print variables

--- Hangman.py
+++ Hangman.py
@@ -1,10 +1,12 @@
 import time
-name = raw_input("What is your name?")
-print"Hello" + name + "Time to play hangman!"
-print""
+
+print("Hello! What is your name?")
+name = input()
+print("Hello, " + name + " Time to play hangman!")
+print("")
 
 time.sleep(1)
-print "Start guessing..."
+print("Start guessing...")
 time.sleep(1)
 
 word = second("secret")
@@ -15,14 +17,14 @@ while turns > 0:
     failed = 0
     for char in word:
         if char in guesses:
-            print char, 
+            print(char) 
         else:
-            print "_"
+            print("_")
             
             failed += 1
             
         if failed == 0:
-            print "You won"
+            print ("You won")
             
             break
         
@@ -31,10 +33,10 @@ while turns > 0:
         guesses += guess
         if guess not in word:
             turns -= 1
-            print"Wrong!"
+            print("Wrong!")
             
-            print "You have", + turns, 'more guesses'
+            print("You have ", + turns, " more guesses")
             
             if turns == 0:
                 
-                print "YOU LOSE YOU ARE LOSER HA"
+                print("YOU LOSE YOU ARE LOSER HA")
