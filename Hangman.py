8fb2d6a625234a646990c6ba1d9d175b9cc0b876
Haebin Yoo <h.yoo@students.chapelfield.org>
2019-06-06 21:04:39 -0400

    Add winning message

--- Hangman.py
+++ Hangman.py
@@ -51,4 +51,5 @@ def main():
             if playerguess not in chosenword:
                 attempts -= 1
                 
-                
+        if "-" not in wordguessed:
+            print(("\nCongratulations! {} was the word").format(chosenword))
