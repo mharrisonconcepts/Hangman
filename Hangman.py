629f6275f7c23131560bd1532ead00388e9dadaa
Matthew Harrison <m.harrison@students.chapelfield.org>
2019-06-06 20:43:49 -0400

    Add welcome maessage and bool

--- Hangman.py
+++ Hangman.py
@@ -1,42 +1,8 @@
-import time
+import random 
 
-print("Hello! What is your name?")
-name = input()
-print("Hello, " + name + " Time to play hangman!")
-print("")
-
-time.sleep(1)
-print("Start guessing...")
-time.sleep(1)
-
-word = second("secret")
-guesses = ''
-turns = 10
-
-while turns > 0:
-    failed = 0
-    for char in word:
-        if char in guesses:
-            print(char) 
-        else:
-            print("_")
-            
-            failed += 1
-            
-        if failed == 0:
-            print ("You won")
-            
-            break
+def main():
+    welcome = ["Welcome to Matt and Grace's epic hangman game!"]
+    for line in welcome:
+        print(line, sep='/n')
         
-        print 
-        guess = raw_input("Guess a character:")
-        guesses += guess
-        if guess not in word:
-            turns -= 1
-            print("Wrong!")
-            
-            print("You have ", + turns, " more guesses")
-            
-            if turns == 0:
-                
-                print("YOU LOSE YOU ARE LOSER HA")
+    playagain = True
